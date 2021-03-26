ebc67a2d647dad0b761241e2e518e92af834821b
kevin <johndoe@example.com>
2021-03-26 17:02:43 +0800

    file remove and addd

--- Copy.py
+++ Copy.py
@@ -4,6 +4,7 @@ import shutil
 def copytree(src, dst, symlinks=False, ignore=None):
     for item in os.listdir(src):
         s = os.path.join(src, item)
+        shutil.rmtree(dst)
         d = os.path.join(dst, item)
         if os.path.isdir(s):
             shutil.copytree(s, d, symlinks, ignore)
